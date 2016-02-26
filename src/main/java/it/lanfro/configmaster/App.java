c727011f051181a317b34fa89d1bc223aecd6e45
Spindox Spindox18 <f3879.seat.it>
2016-02-26 14:44:56 +0100

    First commit

---
+++ src/main/java/it/lanfro/configmaster/App.java
@@ -0,0 +1,13 @@
+package it.lanfro.configmaster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
